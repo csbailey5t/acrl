413091350e54d4fc1b28fc9455458cb2b3b383ce
Scott Bailey <csb5t@virginia.edu>
2021-11-27 11:15:35 -0500

    Add gensim derivatives; add models to streamlit

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 import pandas as pd
+import pyLDAvis
+import pyLDAvis.gensim_models as gensimvis
+
 import seaborn as sns
 import streamlit as st
 
+from streamlit import components
+
 import matplotlib.pyplot as plt
 
+from collections import defaultdict
+from gensim import corpora, models
 from glob import glob
+from nltk.corpus import stopwords
+
+# TODO
+# - [ ] Move cleaning and loading code to a separate Jupyter notebook
 
 
 @st.cache
@@ -13,6 +24,7 @@ def load_data(fns):
     for fn in fns:
         data.append(read_and_clean_data(fn))
     joined_data = pd.concat(data)
+    joined_data.to_csv("data_cleaned/combined.csv", index=False)
     return joined_data
 
 
@@ -36,26 +48,11 @@ def read_and_clean_data(fn):
     return combined
 
 
-def main():
-    st.title("ACRL Conference Analysis")
-
-    fns = glob("data_cleaned/*.xlsx")
-    data = load_data(fns)
-    years = sorted(set(data["year"].tolist()))
-
-    # Sidebar
-    st.sidebar.title("Analysis options")
-    # selected_year = st.sidebar.selectbox(
-    #     "Select the year you're interested in: ", years
-    # )
-
-    # Sidebar search box
-    search_term = st.sidebar.text_input("Search for a word or phrase: ")
-
+def text_search(data, search_term):
     # Filter dataframe based on search term
     if search_term:
         results = data[data["lower_abstract"].str.contains(search_term, na=False)]
-        st.write(results[["year", "id", "title", "Abstract", "lower_abstract"]])
+        st.write(results[["year", "id", "title", "Abstract"]])
 
         data_for_chart = (
             results["year"]
@@ -69,6 +66,67 @@ def main():
         fig = plt.figure(figsize=(10, 10))
         sns.barplot(x="year", y="count", data=data_for_chart)
         st.pyplot(fig)
+
+
+def hdp_model(corpus, dictionary):
+    hdp = models.HdpModel(corpus, id2word=dictionary)
+    hdp_data = gensimvis.prepare(hdp, corpus, dictionary)
+    html_string = pyLDAvis.prepared_data_to_html(hdp_data)
+    components.v1.html(html_string, width=1280, height=1024)
+
+
+def lda_model(corpus, dictionary, num_topics):
+    lda = models.LdaModel(corpus, id2word=dictionary, num_topics=num_topics)
+    lda_data = gensimvis.prepare(lda, corpus, dictionary)
+    html_string = pyLDAvis.prepared_data_to_html(lda_data)
+    components.v1.html(html_string, width=1280, height=1024)
+
+
+def main():
+    st.title("ACRL Conference Analysis")
+
+    # This was the import used also for cleaning; instead let's load cleaned data
+    # fns = glob("data_cleaned/*.xlsx")
+    # data = load_data(fns)
+
+    data = pd.read_csv("data_cleaned/combined.csv")
+    years = sorted(set(data["year"].tolist()))
+
+    # load gensim corpus and dictionary
+    dictionary = corpora.Dictionary.load("acrl.dict")
+    corpus = corpora.MmCorpus("acrl.mm")
+
+    # Sidebar
+    st.sidebar.title("Analysis options")
+
+    # select page
+    page = st.sidebar.selectbox(
+        "Select page", ["Text search", "HDP model", "LDA model"]
+    )
+
+    # selected_year = st.sidebar.selectbox(
+    #     "Select the year you're interested in: ", years
+    # )
+
+    # Sidebar search box
+    search_term = st.sidebar.text_input("Search for a word or phrase: ")
+
+    # sidebar num topics selector
+    num_topics = st.sidebar.slider(
+        "Select the number of topics you're interested in: ",
+        min_value=10,
+        max_value=100,
+        step=5,
+        value=30,
+    )
+
+    # render pages based on page selectbox
+    if page == "Text search":
+        text_search(data, search_term)
+    elif page == "HDP model":
+        hdp_model(corpus, dictionary)
+    elif page == "LDA model":
+        lda_model(corpus, dictionary, num_topics)
 
 
 if __name__ == "__main__":
